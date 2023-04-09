4173c9fe8707cd19dcd25ba5bcd0169c96ce63e6
Bitjockey <bitjockey@jackpoint.network>
2023-04-09 15:24:36 -0400

    Move package assets

--- build-linux.sh
+++ build-linux.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+PACKAGE_ASSETS=packaging-assets/linux
+
 timestamp=$(date +"%Y")
 
 mkdir -p tmp
@@ -6,8 +8,8 @@ mv Diarist.AppDir "tmp/$timestamp.AppDir"
 mv Diarist-*.AppImage "tmp/$timestamp.AppImage"
 
 cp -r build/linux/x64/release/bundle Diarist.AppDir
-cp AppRun Diarist.AppDir/
-cp Diarist.desktop Diarist.AppDir/
-cp diarist.png Diarist.AppDir/
+cp "$PACKAGE_ASSETS/AppRun" Diarist.AppDir/
+cp "$PACKAGE_ASSETS/Diarist.desktop" Diarist.AppDir/
+cp "$PACKAGE_ASSETS/diarist.png" Diarist.AppDir/
 
 appimagetool Diarist.AppDir/
