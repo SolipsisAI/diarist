7eea2d995e0cf777ad98b9a357e87c0de0637177
Bitjockey <bitjockey@jackpoint.network>
2023-04-10 08:05:50 -0400

    Add image

--- lib/utils/helpers.dart
+++ lib/utils/helpers.dart
@@ -1,37 +1,11 @@
 import 'dart:convert';
 import 'package:flutter/services.dart';
-import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
-import 'package:http/http.dart' as http;
 import 'package:uuid/uuid.dart';
 import 'package:intl/intl.dart';
-
-const String loremIpsumApiUrl =
-    'https://litipsum.com/api/dr-jekyll-and-mr-hyde/1/json';
-const int wordsPerMinute = 100;
 
 String randomString() {
   const uuid = Uuid();
   return uuid.v4();
-}
-
-Future<types.TextMessage> randomMessage(types.User user) async {
-  final uri = Uri.parse(loremIpsumApiUrl);
-  final response = await http.get(uri);
-  final json = jsonDecode(response.body) as Map<String, dynamic>;
-  final data = json['text'] as List<dynamic>;
-  final text = data[0];
-  return types.TextMessage(
-    author: user,
-    createdAt: DateTime.now().millisecondsSinceEpoch,
-    id: randomString(),
-    text: text,
-  );
-}
-
-int messageDelay(types.TextMessage message) {
-  final List words = message.text.split(" ");
-  final int wordCount = words.length;
-  return (wordCount ~/ wordsPerMinute) * 60;
 }
 
 int currentTimestamp() {
