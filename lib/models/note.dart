23aafb6a3a4d2bee979ca45b724443eb7a58bbb3
Bitjockey <bitjockey@jackpoint.network>
2023-04-15 10:30:37 -0400

    Remove JSON annotations

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,12 +1,8 @@
 import 'package:realm/realm.dart';
 
-// import 'package:diarist/components/notes_list.dart';
-// import 'package:json_annotation/json_annotation.dart';
-
 part 'note.g.dart';
 
 @RealmModel()
-// @JsonSerializable()
 class _Note {
   @PrimaryKey()
   late final String uuid;
@@ -18,13 +14,8 @@ class _Note {
   late String? emotion = "";
   late String? sentiment = "";
 
-  // _Note();
-
   // NoteItem toItem() {
   //   return NoteItem(
   //       id!, createdAt, updatedAt, title, text, uuid, emotion, sentiment);
   // }
-
-  // factory _Note.fromJson(Map<String, dynamic> json) => _$NoteFromJson(json);
-  // Map<String, dynamic> toJson() => _$NoteToJson(this);
 }
