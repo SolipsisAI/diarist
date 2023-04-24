9c37eaa29c39d760bcfe3ff41cf7f278a9a231e5
Bitjockey <bitjockey@jackpoint.network>
2023-04-23 20:46:08 -0400

    Wrap card in sizedbox

--- lib/views/dashboard_screen.dart
+++ lib/views/dashboard_screen.dart
@@ -9,7 +9,9 @@ class DashboardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Column(
-      children: [DashboardTile(count: notesCount, name: "Notes")],
+      children: [
+        DashboardTile(count: notesCount, name: "Notes"),
+      ],
     ));
   }
 }
@@ -23,13 +25,31 @@ class DashboardTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(children: [
-            Text("$count", style: const TextStyle(fontSize: 50)),
-            Text(name, style: const TextStyle(fontSize: 25)),
-          ])),
-    );
+    return SafeArea(
+        top: false,
+        bottom: false,
+        child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                SizedBox(
+                    child: Card(
+                        clipBehavior: Clip.antiAlias,
+                        child: Padding(
+                            padding: const EdgeInsets.all(30),
+                            child: Semantics(
+                                label: name,
+                                child: Column(
+                                  children: [
+                                    Text("$count",
+                                        style: const TextStyle(
+                                            fontSize: 60,
+                                            fontWeight: FontWeight.bold)),
+                                    Text(name,
+                                        style: const TextStyle(fontSize: 30))
+                                  ],
+                                )))))
+              ],
+            )));
   }
 }
